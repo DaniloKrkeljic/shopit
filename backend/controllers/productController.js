7c1b71ef58564b5f0772a4e3081cfc22f2ca0137
= <=>
2021-01-22 12:33:11 +0100

    first commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,43 @@
+const Product = require('../models/product')
+
+// Create new product => /api/v1/product/new
+exports.newProduct = async (req, res, next) => {
+
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product
+  })
+}
+
+// Get all products => /api/v1/products
+exports.getProducts = async (req,res,next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    count: products.length,
+    products
+  })
+}
+
+// Get single product details => /api/v1/product/:id
+
+exports.getSingleProduct = async (req,res,next) => {
+  
+  try {
+    const product = await Product.findById(req.params.id);
+    res.status(200).json({
+      success: true,
+      product
+    })
+  } catch(e){
+    res.status(404).json({
+      success: false,
+      message: 'No product found under that id'
+    })
+    console.error(e);
+  }
+
+}
