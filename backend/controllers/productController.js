aa43f472f1c80cf88432b1a83f5ef1d600ce0a71
= <=>
2021-01-26 14:37:24 +0100

    trlalal

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product')
+const ErrorHandler = require('../utils/errorHandler');
 
 // Create new product => /api/v1/product/new
 exports.newProduct = async (req, res, next) => {
@@ -26,8 +27,31 @@ exports.getProducts = async (req,res,next) => {
 
 exports.getSingleProduct = async (req,res,next) => {
   
+  const product = await Product.findById(req.params.id);
+  
+  if(!product){
+    return next(new ErrorHandler('Product not found', 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
+  
+
+}
+
+// Update Product => /api/v1/product/:id
+exports.updateProduct = async(req,res,next) => {
+  
   try {
-    const product = await Product.findById(req.params.id);
+    let product = await Product.findById(req.params.id);
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true, 
+      useFindAndModify: false
+    });
+
     res.status(200).json({
       success: true,
       product
@@ -39,5 +63,24 @@ exports.getSingleProduct = async (req,res,next) => {
     })
     console.error(e);
   }
+}
 
+// Delete product by id => /api/v1/admin/product/:id
+exports.deleteProduct = async(req,res,next) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    await product.remove();
+
+    res.status(200).json({
+      success: true,
+      message: 'Product successfully deleted.'
+    })
+  } catch(e){
+    res.status(404).json({
+      success: false,
+      message: 'No product found under that id'
+    })
+    console.error(e);
+  }
 }
